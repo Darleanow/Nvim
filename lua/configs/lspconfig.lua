99392aaf209583490a0a9d79ae8a41cb83d07fba
Enzo Hugonnier <enzohugonn@gmail.com>
2025-09-19 12:46:11 +0200

    feat(LSP): Add python LSP

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -5,36 +5,35 @@ local lspconfig = require "lspconfig"
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- List of LSP servers to load
-local servers = { "html", "cssls", "clangd" }
+local servers = { "html", "cssls", "clangd", "pyright" }
 
 -- Function to enable format on save
 local on_attach = function(client, bufnr)
-    nvlsp.on_attach(client, bufnr) -- Load NvChad’s default on_attach
+  nvlsp.on_attach(client, bufnr) -- Load NvChad’s default on_attach
 
-    -- Enable format on save for clangd
-    if client.server_capabilities.documentFormattingProvider then
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            callback = function()
-                vim.lsp.buf.format({ async = false })
-            end,
-        })
-    end
+  if client.server_capabilities.documentFormattingProvider then
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format { async = false }
+      end,
+    })
+  end
 end
 
 -- Configure LSP servers
 for _, lsp in ipairs(servers) do
-    if lsp == "clangd" then
-        lspconfig.clangd.setup({
-            on_attach = on_attach,
-            on_init = nvlsp.on_init,
-            capabilities = nvlsp.capabilities,
-        })
-    else
-        lspconfig[lsp].setup({
-            on_attach = nvlsp.on_attach,
-            on_init = nvlsp.on_init,
-            capabilities = nvlsp.capabilities,
-        })
-    end
+  if lsp == "clangd" or lsp == "pyright" then
+    lspconfig[lsp].setup {
+      on_attach = on_attach,
+      on_init = nvlsp.on_init,
+      capabilities = nvlsp.capabilities,
+    }
+  else
+    lspconfig[lsp].setup {
+      on_attach = nvlsp.on_attach,
+      on_init = nvlsp.on_init,
+      capabilities = nvlsp.capabilities,
+    }
+  end
 end
